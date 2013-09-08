b3e652f321ed909938030f4f08dce79e4ead3b46
Anja Berens <pharaun666@gmail.com>
2013-09-08 03:29:30 -0700

    Apart of the multi-karma support, the new karma parser is more or less complete, there are still some bugs to work out before we can officially release it as v2

--- KarmaParser/Parser/Karma.hs
+++ KarmaParser/Parser/Karma.hs
@@ -1,12 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Parser.Karma
-    ( KarmaType(..)
-    , Karma(..)
+    ( nickDeFuzzifier
+    , filterBot
     , karmaParse
     , nestedKarmaParse
-    , trialKarmaParse
-    , nickDeFuzzifier
-    , filterBot
     ) where
 
 import qualified Data.Text as T
@@ -24,6 +21,7 @@ import Control.Applicative hiding ((<|>), many, optional)
 import Data.Traversable (sequenceA)
 
 import Parser.Types
+
 
 -- TODO: break this out to the NickName module.
 nickDeFuzzifier :: ParsecT T.Text u Identity T.Text
@@ -44,65 +42,58 @@ filterBot c nick = nick `elem` strictMatchList c || L.any (`T.isPrefixOf` nick) 
 
 
 
-
-
--- Entry point of the old karma parser
+-- TODO:
+--  Clean up and standardize the resulting karma string
+--  - Text.ICU.Normalize
+--  - Normalize it to NFKC
+--  - Casefold(?) or would just lowercase be acceptable
 karmaParse :: Config -> ParsecT T.Text u Identity (Maybe [Karma])
-karmaParse _ = do
-    msg <- many1 (try (anyToken `manyTillIncludesEnd` legacyKarma))
-
-    -- Identify each substring's karma type
-    let karmaTag = map identifyKarma msg
-    let karma = mapMaybe (\a -> case snd a of
-                                    (Just k) -> Just $ Karma k (T.strip $ T.toLower $ T.pack $ fst a)
-                                    Nothing  -> Nothing)
-                    karmaTag
-
-    return $ if null karma
-             then Nothing
-             else Just karma
-
-manyTillIncludesEnd :: (Stream s m t) => ParsecT s u m a -> ParsecT s u m [a] -> ParsecT s u m [a]
-manyTillIncludesEnd p end = scan
+karmaParse conf = processCandidates conf `fmap` nestedKarmaParse conf
     where
-        scan = end <|> do { x <- p; xs <- scan; return (x:xs) }
+        processCandidates :: Config -> [KarmaCandidates] -> Maybe [Karma]
+        processCandidates conf candidates
+            -- If no candidate just exit
+            | L.null candidates = Nothing
 
-legacyKarma :: ParsecT T.Text u Identity String
-legacyKarma = do
-    a <- many1 (choice $ (try . string) `fmap`
-        [ "++"
-        , "--"
-        , "+-"
-        , "-+"
-        , "±"
-        , "∓"
-        , "—" -- For those poor osx users
-        , "＋＋"
-        , "ーー"
-        , "＋ー"
-        , "ー＋"
-        , "⊕⊕"
-        , "⊖⊖"
-        , "⊕⊖"
-        , "⊖⊕"
-        , "⊞⊞"
-        , "⊟⊟"
-        , "⊞⊟"
-        , "⊟⊞"
-        , "ⴱⴱ"
-        , "ⴲⴲ"
-        , "ⴲⴱ"
-        , "ⴱⴲ"
-        ])
-    return $ concat a
+            -- If only one possible candidate just process it and exit early
+            | L.length candidates == 1 = case head candidates of
+                KarmaNonCandidate{} -> Nothing
+                KarmaCandidate{kcMessage=msg, kcKarma=karma} ->
+                    case evalulateKarma conf karma of
+                        (e, Just k)  -> Just [Karma k (T.toLower $ T.pack $ msg ++ e)] -- TODO: add T.strip maybe
+                        (_, Nothing) -> Nothing
+
+            -- TODO: Process the rest here
+            | otherwise = do
+                Nothing
 
 
+-- Evalulate in a rightmost manner, and return the remainder of the karma
+evalulateKarma :: Config -> String -> (String, Maybe KarmaType)
+evalulateKarma conf karma
+    | L.null karma        = ("", Nothing)
+    | L.length karma == 1 = lookupTotalKarma conf karma
+    | otherwise           = lookupPartialKarma conf karma
 
+    where
+        lookupTotalKarma conf karma =
+            let tk = L.lookup (L.head $ L.reverse karma) $ totalKarma conf
+            in case tk of
+                Just k  -> (L.take (L.length karma - 1) karma, Just k)
+                Nothing -> ("", Nothing)
 
+        lookupPartialKarma conf karma =
+            let pk  = L.lookup (L.head $ L.reverse karma) $ partialKarma conf
+                pk' = L.lookup (L.head $ L.tail $ L.reverse karma) $ partialKarma conf
+            in case (pk, pk') of
+                (Nothing, _) -> lookupTotalKarma conf karma
+                (Just _, Nothing) -> lookupTotalKarma conf (L.init karma)
+                (Just k, Just k') -> (L.take (L.length karma - 2) karma, Just $ partalToTotal k k')
 
+        partalToTotal Up Up = Upvote
+        partalToTotal Down Down = Downvote
+        partalToTotal _ _ = Sidevote
 
-
--- Protype of the new karma parser
 
 -- Brace choice Breakdown:
 --  - From a corpus of 25,000 irc messages
@@ -114,14 +105,12 @@ legacyKarma = do
 -- "" - 982  - 549037
 -- () - 1570 - 1013058
 -- '' - 1691 - 1602736
-
--- TODO: decide what brace we want to use
 leftBrace :: Config -> ParsecT T.Text u Identity Char
 leftBrace = char . openBrace
 
--- TODO: decide what brace we want to use
 rightBrace :: Config -> ParsecT T.Text u Identity Char
 rightBrace = char . closeBrace
+
 
 -- Karma
 -- Check for one total karma
@@ -185,108 +174,3 @@ nestedKarmaParse conf = concat `fmap` many1 (choice
         , try (eatKarmaInsideBracesParse conf)
         , fmap pure nonKarmaParse
         ])
-
-
--- TODO: Finally take the KarmaCandidate list and process then to determite which ones are
---  ACTUAL Karma results to be emitted to stdout
-trialKarmaParse :: Config -> ParsecT T.Text u Identity (Maybe [Karma])
-trialKarmaParse conf = processCandidates conf `fmap` nestedKarmaParse conf
-    where
-        processCandidates :: Config -> [KarmaCandidates] -> Maybe [Karma]
-        processCandidates conf candidates
-            -- If no candidate just exit
-            | L.null candidates = Nothing
-
-            -- If only one possible candidate just process it and exit early
-            | L.length candidates == 1 = case head candidates of
-                KarmaNonCandidate{} -> Nothing
-                KarmaCandidate{kcMessage=msg, kcKarma=karma} ->
-                    case evalulateKarma conf karma of
-                        (e, Just k)  -> Just [Karma k (T.pack $ msg ++ e)]
-                        (_, Nothing) -> Nothing
-
-            -- Process the rest here
-            | otherwise = do
-                Nothing
-
--- Evalulate in a rightmost manner, and return the remainder of the karma
-evalulateKarma :: Config -> String -> (String, Maybe KarmaType)
-evalulateKarma conf karma
-    | L.null karma        = ("", Nothing)
-    | L.length karma == 1 = lookupTotalKarma conf karma
-    | otherwise           = lookupPartialKarma conf karma
-
-    where
-        lookupTotalKarma conf karma =
-            let tk = L.lookup (L.head $ L.reverse karma) $ totalKarma conf
-            in case tk of
-                Just k  -> (L.take (L.length karma - 1) karma, Just k)
-                Nothing -> ("", Nothing)
-
-        lookupPartialKarma conf karma =
-            let pk  = L.lookup (L.head $ L.reverse karma) $ partialKarma conf
-                pk' = L.lookup (L.head $ L.tail $ L.reverse karma) $ partialKarma conf
-            in case (pk, pk') of
-                (Nothing, _) -> lookupTotalKarma conf karma
-                (Just _, Nothing) -> lookupTotalKarma conf (L.init karma)
-                (Just k, Just k') -> (L.take (L.length karma - 2) karma, Just $ partalToTotal k k')
-
-        partalToTotal Up Up = Upvote
-        partalToTotal Down Down = Downvote
-        partalToTotal _ _ = Sidevote
-
-
--- Clean up and standardize the resulting karma string
---  - Text.ICU.Normalize
---  - Normalize it to NFKC
---  - Casefold(?) or would just lowercase be acceptable
-
-
-
-
-identifyKarma :: String -> (String, Maybe KarmaType)
-identifyKarma msg =
-    if T.null $ T.strip $ T.pack msg
-    then (msg, Nothing)
-    else
-        let (m1, k1) = (init msg, last msg) in
-        if T.null $ T.strip $ T.pack m1
-        then (m1, Nothing)
-        else case singleKarmaType k1 of
-            (Just k) -> (m1, Just k)
-            Nothing  -> do
-                let (m2, k2) = L.splitAt (L.length msg - 2) msg
-                if T.null $ T.strip $ T.pack m2
-                then (m2, Nothing)
-                else (m2, karmaType k2)
-
--- TODO: unify the karmaType, singleKarmaType and karma parser to use a unified karma Type database
---  Externalize this to a configuration file, can probably do - list of - up/down/side, and upvote/sidevote/downvote and then process them.
-karmaType :: String -> Maybe KarmaType
-karmaType "++"   = Just Upvote
-karmaType "--"   = Just Downvote
-karmaType "+-"   = Just Sidevote
-karmaType "-+"   = Just Sidevote
-karmaType "＋＋" = Just Upvote
-karmaType "ーー" = Just Downvote
-karmaType "＋ー" = Just Sidevote
-karmaType "ー＋" = Just Sidevote
-karmaType "⊕⊕"   = Just Upvote
-karmaType "⊖⊖"   = Just Upvote
-karmaType "⊕⊖"   = Just Sidevote
-karmaType "⊖⊕"   = Just Sidevote
-karmaType "⊞⊞"   = Just Upvote
-karmaType "⊟⊟"   = Just Upvote
-karmaType "⊞⊟"   = Just Sidevote
-karmaType "⊟⊞"   = Just Sidevote
-karmaType "ⴱⴱ"   = Just Upvote
-karmaType "ⴲⴲ"   = Just Upvote
-karmaType "ⴲⴱ"   = Just Sidevote
-karmaType "ⴱⴲ"   = Just Sidevote
-karmaType _      = Nothing
-
-singleKarmaType :: Char -> Maybe KarmaType
-singleKarmaType '±' = Just Sidevote
-singleKarmaType '∓' = Just Sidevote
-singleKarmaType '—' = Just Downvote
-singleKarmaType _   = Nothing
--- KarmaParser/karma-test.hs
+++ KarmaParser/karma-test.hs
@@ -9,8 +9,6 @@ import Text.Parsec
 import Parser.Karma
 import Parser.Types
 
-nick n = either (\_ -> Nothing) (\a -> Just a) (parse nickDeFuzzifier "(stdin)" $ T.pack n)
-
 main :: IO ()
 main = do
     putStrLn "Nickname tests:"
@@ -36,7 +34,7 @@ main = do
 -- Nick name Defuzzifier tests
 --
 buildNickTests = TestList . map (\(src, dst) -> TestLabel (src ++ " -> " ++ dst) (nickTest src dst))
-nickTest src dst = TestCase (assertEqual ("Nickname DeFuzzifier: " ++ src ++ " -> " ++ dst) (Just $ T.pack dst) (nick src))
+nickTest src dst = TestCase (assertEqual "" (Just $ T.pack dst) (either (\_ -> Nothing) (\a -> Just a) (parse nickDeFuzzifier "(stdin)" $ T.pack src)))
 
 nickData :: [(String, String)]
 nickData =
@@ -80,11 +78,6 @@ botData =
     ]
 
 --
--- KarmaType quickcheck
---
-
-
---
 -- NewKarmaType test cases
 --
 makeKarmaConfig = Config [] [] [('+', Up), ('-', Down)] [('±', Sidevote), ('∓', Sidevote)] '(' ')'
@@ -97,6 +90,8 @@ newKarmaData =
     , ("a++", [KarmaCandidate "a" "++"])
     , ("a++++", [KarmaCandidate "a" "++++"])
     , ("a b++", [KarmaCandidate "a b" "++"])
+    , ("a+ b++", [KarmaCandidate "a+ b" "++"])
+    , ("a+b++", [KarmaCandidate "a+b" "++"])
     , ("a++ b", [KarmaCandidate "a" "++", KarmaNonCandidate " b"])
     , ("a++ b++", [KarmaCandidate "a" "++", KarmaCandidate " b" "++"])
     , ("(a)++", [KarmaCandidate "a" "++"])
@@ -170,7 +165,7 @@ newKarmaData =
 --
 makeKarmaParseConfig = Config [] [] [('+', Up), ('-', Down)] [('±', Sidevote), ('∓', Sidevote)] '(' ')'
 buildKarmaTests = TestList . map (\(src, dst) -> TestLabel "" (karmaTest src dst))
-karmaTest str result = TestCase (assertEqual "" (if L.null result then Nothing else Just result) (either (\_ -> Nothing) (\a -> a) (parse (trialKarmaParse $ makeKarmaParseConfig) "(stdin)" $ T.pack str)))
+karmaTest str result = TestCase (assertEqual "" (if L.null result then Nothing else Just result) (either (\_ -> Nothing) (\a -> a) (parse (karmaParse $ makeKarmaParseConfig) "(stdin)" $ T.pack str)))
 
 
 karmaData :: [(String, [Karma])]
@@ -180,124 +175,98 @@ karmaData =
     , ("a+", [])
     , ("a-", [])
 
+    -- TODO: do we want to disallow this case
+    , ("++", [])
+
     -- Parse trivial karma
     , ("a++", [Karma Upvote "a"])
-    , ("a--" [Karma Downvote "a"])
-    , ("a+-" [Karma Sidevote "a"])
-    , ("a-+" [Karma Sidevote "a"])
+    , ("a--", [Karma Downvote "a"])
+    , ("a+-", [Karma Sidevote "a"])
+    , ("a-+", [Karma Sidevote "a"])
     , ("a±", [Karma Sidevote "a"])
 
     -- Parse excess karma
+    , ("a+++", [Karma Upvote "a+"])
+    , ("a-++", [Karma Upvote "a-"])
+    , ("a++++", [Karma Upvote "a++"])
+    , ("a--++", [Karma Upvote "a--"])
+    , ("++--", [Karma Downvote "++"])
+    , ("--++", [Karma Upvote "--"])
+
+    , ("a±±", [Karma Sidevote "a±"])
+    , ("a+±", [Karma Sidevote "a+"])
+
+    -- TODO: not sure about this one, should this results into an karma or should we fail?
+    , ("a+±+", [Karma Sidevote "a+"])
+
+    -- Whitespace karma
+    , ("a b++", [Karma Upvote "a b"])
+    , ("a+ b++", [Karma Upvote "a+ b"])
+
+    -- Braced karma
+    , ("(a)++", [Karma Upvote "a"])
+    , ("(a++)++", [Karma Upvote "a++"])
+    , ("(a++", [Karma Upvote "(a"])
+    , ("a)++", [Karma Upvote "a)"])
+    , ("(a) b++", [Karma Upvote "(a) b"])
+
+    -- Nested braces
+    , ("((a))++", [Karma Upvote "(a)"])
+    , ("((a++))++", [Karma Upvote "(a++)"])
+    , ("((a++)++", [Karma Upvote "(a++"])
+    , ("(a++))++", [Karma Upvote "a++)"])
+    , ("((a)) b++", [Karma Upvote "((a)) b"])
+    , ("(a (b))++", [Karma Upvote "a (b)"])
+    , ("(++a)++", [Karma Upvote "++a"])
+    , ("a(b)c++", [Karma Upvote "a(b)c"])
+    , ("((a)++)++", [Karma Upvote "(a)++"])
+
+    -- White space in braces
+    , ("( a)++", [Karma Upvote " a"])
+    , ("(a )++", [Karma Upvote "a "])
+    , ("( a )++", [Karma Upvote " a "])
+    , ("((a) )++", [Karma Upvote "(a) "])
+    , ("( (a))++", [Karma Upvote " (a)"])
+    , ("( (a) )++", [Karma Upvote " (a) "])
+    , ("((a)++ )++", [Karma Upvote "(a)++ "])
+    , ("((a++)++ )++", [Karma Upvote "(a++)++ "])
+
+    -- Braced non-candidates
+    , ("(a)", [])
+    , ("(a++)", [])
+    , ("((a))", [])
+    , ("((a++))", [])
+    , ("((a)++)", [])
+    , ("a (b++)", [])
+    , ("(a b++)", [])
+    , ("(a (b++))", [])
+
+    -- Unicode
+    , ("私を++", [Karma Upvote "私を"])
+    , ("♥++", [Karma Upvote "♥"])
+    , ("とある.NET++", [Karma Upvote "とある.net"])
+
+    -- Unusual
+    , ("/\\.?code/i--", [Karma Downvote "/\\.?code/i"])
+
+    -- TODO: Decide how to deal with karma trimming such as
+    -- "a ++" -> "a"++ for example.
     ]
 
 
---    , ("a++++", [KarmaCandidate "a" "++++"])
---    , ("a+++", [KarmaCandidate "a" "+++"])
---    , ("a+±+", [KarmaCandidate "a" "+±+"])
---    , ("a+±", [KarmaCandidate "a" "+±"])
---    , ("a±", [KarmaCandidate "a" "±"])
-
---    , ("a b++", [KarmaCandidate "a b" "++"])
---    , ("(a)++", [KarmaCandidate "a" "++"])
---    , ("(a++)++", [KarmaCandidate "a++" "++"])
---    , ("(a++", [KarmaCandidate "(a" "++"])
---    , ("a)++", [KarmaCandidate "a)" "++"])
---    , ("((a))", [KarmaNonCandidate "((a))"])
---    , ("(a) b++", [KarmaCandidate "(a) b" "++"])
---    , ("((a)) b++", [KarmaCandidate "((a)) b" "++"])
---
---    -- TESTS: uncertain about behavors -- Probably better to only parse one set of braces
---    , ("((a))++", [KarmaCandidate "(a)" "++"])
---    , ("(a (b))++", [KarmaCandidate "a (b)" "++"])
---    , ("((a (b)))++", [KarmaCandidate "(a (b))" "++"])
---    , ("((a) b))++", [KarmaCandidate "(a) b)" "++"])
---
---    -- TESTS: Should extend the parser to care about karma inside braces only if there's no karma outside braces?
---    -- Maybe a nice way to "escape" karma without karma'ing
---    , ("(a++)", [KarmaNonCandidate "(a++)"])
---    , ("((a++))", [KarmaNonCandidate "((a++))"])
---    , ("a (b++)", [KarmaNonCandidate "a (b++)"])
---    , ("a ((b++))", [KarmaNonCandidate "a ((b++))"])
---
---    -- Concat/preinc karma
---    , ("(++a)++", [KarmaCandidate "++a" "++"])
---
---    -- Brace whitespaces
---    , ("( a)++", [KarmaCandidate " a" "++"])
---    , ("(a )++", [KarmaCandidate "a " "++"])
---    , ("( a )++", [KarmaCandidate " a " "++"])
---    , ("( (a) )++", [KarmaCandidate " (a) " "++"])
---    , ("((a) )++", [KarmaCandidate "(a) " "++"])
---    , ("( (a))++", [KarmaCandidate " (a)" "++"])
---
---    -- Complicated brace/karma nesting
---    , ("((a)++)++", [KarmaCandidate "(a)++" "++"])
---    , ("((a)++ )++", [KarmaCandidate "(a)++ " "++"])
---    , ("((a++)++ )++", [KarmaCandidate "(a++)++ " "++"])
 
 
 
 
-
-
-
---    [ ("", [])
---    [ ("a++", [Karma Upvote "a"])
---    , ("a--", [Karma Downvote "a"])
---    , ("a+-", [Karma Sidevote "a"])
---    , ("a-+", [Karma Sidevote "a"])
---
---    -- Multi-karma cases
---    , ("a++", [Karma Upvote "a"])
---    , ("a+++", [Karma Upvote "a"])
---    , ("a++++", [Karma Upvote "a++"])
---    , ("a+++++", [Karma Upvote "a++"])
---    , ("++--", [Karma Downvote "++"])
---    , ("--++", [Karma Upvote "--"])
+--    , ("a: ++", [Karma Upvote "a"])
 --
 --    -- Multi-karma in one expression
 --    , ("a++ a++", [Karma Upvote "a", Karma Upvote "a"])
 --    , ("a++ b++", [Karma Upvote "a", Karma Upvote "b"])
 --    , ("ikea++ modular furniture++ meatballs++", [Karma Upvote "ikea", Karma Upvote "modular furniture", Karma Upvote "meatballs"])
---    , ("a: ++", [Karma Upvote "a"])
 --    , ("btw, a++ and b++ for this", [Karma Upvote "a", Karma Upvote "b"])
 --    , ("Solved. a++, b++", [Karma Upvote "a", Karma Upvote "b"])
 --
---    -- Karma within braces and quotations
---    , ("(a b)++", [Karma Upvote "a b"])
---    , ("{a b}++", [Karma Upvote "a b"])
---    , ("[a b]++", [Karma Upvote "a b"])
---    , ("<a b>++", [Karma Upvote "a b"])
---    , ("'a b'++", [Karma Upvote "a b"])
---    , ("`a b`++", [Karma Upvote "a b"])
---    , ("\"a b\"++", [Karma Upvote "a b"])
---    , ("(a--)++", [Karma Upvote "a--"])
---    , ("mac'n'cheese++", [Karma Upvote "mac'n'cheese"])
---
---    , ("(a b++", [Karma Upvote "(a b"])
---    , ("{a b++", [Karma Upvote "{a b"])
---    , ("[a b++", [Karma Upvote "[a b"])
---    , ("<a b++", [Karma Upvote "<a b"])
---    , ("'a b++", [Karma Upvote "'a b"])
---    , ("`a b++", [Karma Upvote "`a b"])
---    , ("\"a b++", [Karma Upvote "\"a b"])
---
---    , ("a b)++", [Karma Upvote "a b)"])
---    , ("a b}++", [Karma Upvote "a b}"])
---    , ("a b]++", [Karma Upvote "a b]"])
---    , ("a b>++", [Karma Upvote "a b>"])
---    , ("a b'++", [Karma Upvote "a b'"])
---    , ("a b`++", [Karma Upvote "a b`"])
---    , ("a b\"++", [Karma Upvote "a b\""])
---
---    -- Unicode
---    , ( "私を++", [Karma Upvote "私を"])
---    , ( "♥++", [Karma Upvote "♥"])
---    , ("とある.NET++", [Karma Upvote "とある.NET"])
---
---    -- Unusual
---    , ( "++", [])
---    , ("/\\.?code/i—", [Karma Downvote "/\\.?code/i—"])
 --
 --    -- "Clearly" Invalid karma which should result in nothing
 --    , (":,::--_._,:---.", [])
@@ -324,34 +293,34 @@ karmaData =
 --    , ("-rw-r--r-- 1 user users 215k 2012-08-14 12:13 something/path", [])
 --    , ("Fri: -15f--4F | sat: -31F--34F", [])
 --    , ("http://www.something/Salad-Fingers-Episode-5--Salad-Fingers--Picnic.jpg", [])
+
 -- that definitely doesn't merit a --
 -- >8-D-|--<
 -- that sounds like it should prevent ++a++ too
 
-    -- Nickname upvotes in various form
-    --
-    -- Awesome! Nick++
-    -- Nick++ testing stuff
-    -- Nick1++ and Nick2++
-    -- Nick++!!
-    -- Ok then. Nick++
-    -- ＳＮＯＯＰ ＬＩＯＮ++
+-- Nickname upvotes in various form
+--
+-- Awesome! Nick++
+-- Nick++ testing stuff
+-- Nick1++ and Nick2++
+-- Nick++!!
+-- Ok then. Nick++
+-- ＳＮＯＯＰ ＬＩＯＮ++
 
-    -- Nickname with topic
-    --
-    -- Nick1, Nick2: good bread++
-    -- Nick: sounds good. Push++
-    -- Nick: karma++
+-- Nickname with topic
+--
+-- Nick1, Nick2: good bread++
+-- Nick: sounds good. Push++
+-- Nick: karma++
 
-    -- Braces
-    --
-    -- ""ergonomic" keyboards"++
-    -- (socialist stroopwafel) policies+-
-    -- socialist (stroopwafel policies)++
-    -- commun(ity)ism++
-    -- dev<prime_number>++
-    -- ObjectionalLanguage: "Nine years of ballet, asshole."++
-    -- (not using --no-notification)--
+-- Braces
+--
+-- ""ergonomic" keyboards"++
+-- (socialist stroopwafel) policies+-
+-- socialist (stroopwafel policies)++
+-- commun(ity)ism++
+-- dev<prime_number>++
+-- ObjectionalLanguage: "Nine years of ballet, asshole."++
+-- (not using --no-notification)--
 
-    -- Warning: Sorry, can not save your changes. This ticket has been modified by someone else since you started--
---    ]
+-- Warning: Sorry, can not save your changes. This ticket has been modified by someone else since you started--
