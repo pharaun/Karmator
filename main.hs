d04991c31645b3710f0827a909fe539aacf3050c
Anja Berens <pharaun666@gmail.com>
2014-03-02 21:32:25 -0800

    Connect using simple tls like the other connect loop

--- main.hs
+++ main.hs
@@ -3,24 +3,15 @@ import Data.List
 import Network
 import System.IO
 import System.Time
-import System.Exit
 import Control.Monad.Reader
 import Control.Monad.Trans.State.Strict
-import Control.Exception
-import Text.Printf
-import Prelude hiding (catch, log)
+import Prelude hiding (log)
 import Data.Maybe
-import Data.Default.Class
-
-import Data.Functor ((<$>))
-import Data.List (head)
 
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.Text as T
 
-import qualified Pipes.ByteString as PBS
 import qualified Pipes.Network.TCP as PNT
 import qualified Pipes.Attoparsec as PA
 import qualified Pipes.Prelude as PP
@@ -32,18 +23,16 @@ import Control.Applicative
 import Data.Attoparsec.ByteString
 
 -- TLS
-import qualified Network.TLS as TLS
-import qualified Network.TLS.Extra as TLS
+import qualified Pipes.Network.TCP.TLS as TLS
+import qualified Network.Simple.TCP.TLS as TLS
 import qualified System.X509.Unix as TLS
-import qualified Network.Socket as NS
-import qualified Network.Socket.ByteString as NSB
-import qualified Crypto.Random.AESCtr as RNG
+import qualified Network.TLS as TLS
 
 
 -- Per server config for the bot
 data ServerConfig = ServerConfig
     { server :: String
-    , port :: NS.PortNumber
+    , port :: PortNumber
     , nicks :: [BS.ByteString] -- First one then alternatives in descending order
     , userName :: BS.ByteString
     , serverPassword :: Maybe BS.ByteString
@@ -95,6 +84,39 @@ data ServerState = ServerState
     -- Debugging/initial test impl
     , starttime :: ClockTime
     }
+
+
+--
+-- Establish TLS connection
+--
+establishTLS :: ServerConfig -> ServerPersistentState -> IO ()
+establishTLS sc sps = PNT.withSocketsDo $
+    -- Establish the logfile
+    withFile (logfile sc) AppendMode (\l -> do
+
+        -- Set log to be unbuffered for testing
+        hSetBuffering l NoBuffering
+
+        -- Establish the client configuration
+        params <- TLS.makeClientSettings Nothing (server sc) (show $ port sc) True <$> TLS.getSystemCertificateStore
+
+        -- Workaround the dns issue in simple tcp
+--        let params' = params { TLS.clientServerIdentification = ("chat.freenode.net", C8.pack $ show $ port sc) }
+
+        -- Stablish connection
+        TLS.connect params (server sc) (show $ port sc) (\(context, _) -> do
+            -- Session start time
+            t <- getClockTime
+
+            -- Initial connection
+            runEffect $ handshake sc sps >-> showMessage >-> log l >-> TLS.toContext context
+
+            -- Regular irc streaming
+            runEffect $ ircParserErrorLogging l (TLS.fromContext context >-> log l) >-> command t >-> showMessage >-> log l >-> TLS.toContext context
+
+            return ()
+            )
+        )
 
 
 --
@@ -283,115 +305,3 @@ message  = do
     _ <- IRC.crlf
     return $ IRC.Message p c ps
 --------------------------------------------------------------------------
-
-
--- Break this out into its own file and revert/use the
--- Pipes.Network.TCP.TLS package when its working again
---------------------------------------------------------------------------
-
-
-
-
-
-
-
---
--- Establish TLS connection
---
-establishTLS :: ServerConfig -> ServerPersistentState -> IO ()
-establishTLS sc sps = PNT.withSocketsDo $
-    -- Establish the logfile
-    withFile (logfile sc) AppendMode (\l -> do
-        -- Set log to be unbuffered for testing
-        hSetBuffering l NoBuffering
-
-        -- Establish the client configuration
-        params <- createClientParams (server sc) (port sc)
-
-        -- Establish a socket
-        socket <- connect (server sc) (port sc)
-
-        -- Generate ctx
-        rng <- RNG.makeSystem
-        ctx <- TLS.contextNew socket params rng
-
-        -- Hook into the ctx for logging
-        TLS.contextHookSetLogging ctx $ customLogging l
-
-        -- TLS handshake
-        TLS.handshake ctx
-
-        -- Network stuff
-
-        TLS.bye ctx
-
-
---        TLS.connect def' (server sc) (show $ port sc) (\(context, _) -> do
---                -- Session start time
---                t <- getClockTime
---
---                -- Initial connection
---                runEffect $ handshake sc sps >-> showMessage >-> log l >-> TLS.toContext context
---
---                -- Regular irc streaming
---                runEffect $ ircParserErrorLogging l (TLS.fromContext context >-> log l) >-> command t >-> showMessage >-> log l >-> TLS.toContext context
---
---                return ()
---            )
-        )
-
---
--- Client Configuration
---
-createClientParams :: HostName -> NS.PortNumber -> IO TLS.ClientParams
-createClientParams host port = (\store -> return $ (TLS.defaultParamsClient host (C8.pack $ show port))
-    { TLS.clientSupported = def
-        { TLS.supportedCiphers = ciphers_AES_CBC
-        }
-    , TLS.clientShared = def
-        { TLS.sharedCAStore = store
-        }
-    }) =<< TLS.getSystemCertificateStore -- TODO: inclusion of this seems to freeze the network code
-    where
-        ciphers_AES_CBC :: [TLS.Cipher]
-        ciphers_AES_CBC =
-            [ TLS.cipher_AES256_SHA256
-            , TLS.cipher_AES256_SHA1
-            , TLS.cipher_AES128_SHA256
-            , TLS.cipher_AES128_SHA1
-            ]
-
---
--- Socket connection
--- TODO: error handling
---
-connect :: HostName -> NS.PortNumber -> IO NS.Socket
-connect hostname dstPort = do
-    -- Set up the hints to prefer ipv4 udp datagrams
-    let myAddr = NS.defaultHints
-            { NS.addrFamily = NS.AF_INET
-            , NS.addrSocketType = NS.Stream
-            }
-
-    -- Looks up hostname and port then grab the first
-    addrInfo <- head <$> NS.getAddrInfo (Just myAddr) (Just hostname) (Just $ show dstPort)
-
-    -- Establish a socket for communication
-    sock <- NS.socket (NS.addrFamily addrInfo) NS.Stream NS.defaultProtocol
-
-    -- Connect
-    NS.connect sock (NS.addrAddress addrInfo)
-
-    -- Save off the socket, program name, and server address in a handle
-    return sock
-
---
--- TLS logging
---
-customLogging :: Handle -> TLS.Logging
-customLogging l = TLS.Logging
-    { TLS.loggingPacketSent = \m -> BS.hPutStr l $ BS.concat ["debug: >> ", C8.pack m, "\n"]
-    , TLS.loggingPacketRecv = \m -> BS.hPutStr l $ BS.concat ["debug: << ", C8.pack m, "\n"]
-    , TLS.loggingIOSent     = (\_ -> return ())
-    , TLS.loggingIORecv     = (\_ _ -> return ())
-    }
