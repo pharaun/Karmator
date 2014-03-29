7769622eb9b915c1e128c7e486ca772abd14f31f
Anja Berens <pharaun666@gmail.com>
2014-03-29 14:39:00 -0700

    Started to modulize the database code, need to clean up and create a Karmator Plugin

--- src/Database.hs
+++ src/Database.hs
@@ -1,4 +1,15 @@
 {-# LANGUAGE EmptyDataDecls, FlexibleContexts, GADTs, OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+module Database
+    ( KarmaTable(..)
+    , allKarma
+    , partalKarma
+    , topNDenormalized
+    , rankingDenormalized
+    , countK
+    , topNSideVotesDenormalized
+    , sideVotesRankingDenormalized
+
+    ) where
 
 import qualified Database.Persist as P
 import qualified Database.Persist.Sqlite as P
@@ -9,8 +20,6 @@ import Data.Text (Text)
 import qualified Data.Text as T
 
 import Data.Time.Clock (UTCTime)
-import Data.Conduit (($$))
-import qualified Data.Conduit.List as CL
 import Control.Monad.IO.Class
 
 import Data.Int
@@ -137,34 +146,3 @@ rankingDenormalizedT karmaName karmaTotal whom =
 --             .values(by_whom_name=by_whom_name, for_what_name=kind['message'],
 --                     amount=vote_amount_map[kind['karma_type']]))
 --        session.execute(q)
-
-
-
-main :: IO ()
-main = P.runSqlite "test2.db" $ do
-
-    vote1a <- allKarma KarmaReceived
-    vote1b <- allKarma KarmaGiven
-
-    vote3a <- partalKarma KarmaReceived ["nishbot", "doesnotexist"]
-    vote3b <- partalKarma KarmaGiven ["nishbot", "doesnotexist"]
-
-    vote4a <- topNDenormalized KarmaReceived 3 desc
-    vote4b <- topNDenormalized KarmaGiven 3 desc
-    vote4c <- topNDenormalized KarmaReceived 3 asc
-    vote4d <- topNDenormalized KarmaGiven 3 asc
-
-    vote5a <- rankingDenormalized KarmaReceived "nishbot"
-    vote5b <- rankingDenormalized KarmaGiven "nishbot"
-
-    vote6a <- countK KarmaReceived "nishbot"
-    vote6b <- countK KarmaGiven "nishbot"
-
-    vote7a <- topNSideVotesDenormalized KarmaReceived 3
-    vote7b <- topNSideVotesDenormalized KarmaGiven 3
-
-    vote8a <- sideVotesRankingDenormalized KarmaReceived "nishbot"
-    vote8b <- sideVotesRankingDenormalized KarmaGiven "nishbot"
-
-    liftIO $ Prelude.mapM_ (print . show) vote8a
---    liftIO $ (print . show) vote11
