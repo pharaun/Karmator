0ae2da3fc847eb41ae1f2bc68e2427a8c8b57675
Anja Berens <pharaun666@gmail.com>
2014-08-19 00:35:11 -0700

    standardized onto a api... roughly, need lot of tweaks/cleanup work

--- src/DynTest.hs
+++ src/DynTest.hs
@@ -31,10 +31,86 @@ import System.IO.Unsafe
 import Language.Haskell.TH hiding (match)
 import Language.Haskell.TH.Syntax
 
-moduleName = fmap loc_module qLocation >>= \mod -> return (AppE (VarE (mkName "Data.ByteString.Char8.pack")) (LitE (StringL mod)))
+moduleNameTH = fmap loc_module qLocation >>= \mod -> return (AppE (VarE (mkName "Data.ByteString.Char8.pack")) (LitE (StringL mod)))
 -- TEMPLATE HASKELL
 
 type Msg = String
+
+
+
+--
+-- Final
+--
+
+-- Final version that trys to incorporate all of the nice features of all
+-- of the other versions
+--
+data Command m st = Command -- TODO: the st maybe better off as State monad so they can pull/update the state if needed
+    { commandHelp  :: st -> Msg -> m [Msg] -- TODO: uncertain, this should be as simple as possible
+    , commandMatch :: st -> Msg -> m Bool
+    , commandEval  :: st -> Msg -> m (st, Maybe [Msg])
+    }
+
+-- Deals in serialization of the state for the plugin
+-- TODO: customize it so that I can provide some default serialization such as json, etc
+-- TODO: look into doing something like this below v to automate serialization (ie read/show)
+--
+---- | Existential type to store a state extension.
+--data StateExtension =
+--    forall a. ExtensionClass a => StateExtension a
+--    -- ^ Non-persistent state extension
+--  | forall a. (Read a, Show a, ExtensionClass a) => PersistentExtension a
+--    -- ^ Persistent extension
+--
+data Serial st = Serial
+    { serialize   :: st -> Maybe B.ByteString
+    , deserialize :: B.ByteString -> Maybe st
+    }
+
+-- Module that contains the plugins
+data Module m st = Module
+    { moduleSerialize :: m (Maybe (Serial st))
+    , moduleCmds :: m [Command m st]
+
+    -- TODO: not certain how to handle init/exit yet
+    --, pluginDefState  = return $ error "State not initalized"
+    , moduleDefState :: m st
+
+    -- TODO: this should be handled by some template haskell maybe
+    , moduleName :: String
+    }
+
+
+-- List of existional qualified plugins
+data Plugin = forall st. Plugin
+    { _pthis  :: st
+    , _pinit  :: Maybe B.ByteString -> st
+    , _psave  :: st -> Maybe B.ByteString
+    , _pmatch :: Msg -> st -> Bool
+    , _peval  :: Msg -> st -> (st, Maybe Msg)
+    }
+
+
+--
+-- This registers a module defined above into a format suitable for
+-- processing by the ircbot in a generic manner
+--
+loadModule :: (Monad m, MonadIO m) => Module m st -> m [CommandRef] [ModuleRef]
+loadModule = undefined
+
+
+
+
+-- TODO:
+--  - Find a way to share state between a CommandRef and ModuleRef list so
+--      that we can iterate through commandref and check for matches + run
+--      commands, but upon init & shutdown we can hit ModuleRef to init/cleanup
+--  - There maybe a better approach such as a single list that holds all of
+--      the needed info and building a nice ... transversable interface for
+--      transversing the list for modules vs commands
+
+
+
 
 
 --
@@ -88,184 +164,8 @@ uptimePlug = emptyPlug uptimeInit uptimeSave uptimeMatch uptimeEval
 
 
 
---
--- Generation Three
---
-
--- Plugin Monad
-newtype PM a = PM { runPM :: ReaderT (String) IO a }
-    deriving (Functor, Applicative, Monad, MonadIO, MonadException)
-
--- | This transformer encodes the additional information a module might
---   need to access its name or its state.
-newtype PluginT st m a = PluginT { runPluginT :: ReaderT (MVar st, B.ByteString) m a }
-    deriving (Applicative, Functor, Monad, MonadTrans, MonadIO, MonadException)
-
--- Deals in serialization of the state for the plugin
--- Consider doing some serialization with acid-state
-data Serial st = Serial
-    { serialize   :: st -> Maybe B.ByteString
-    , deserialize :: B.ByteString -> Maybe st
-    }
-
--- Deals with pattern matching a match to a eval
-data Command m = Command
-    { commandName  :: String
-    , commandHelp  :: Msg -> WriterT [Msg] m ()
-    , commandMatch :: Msg -> m Bool
-    , commandEval  :: Msg -> WriterT [Msg] m ()
-    }
-
--- Plugin structure, for a collection of shared commands
--- TODO: Maybe the monadic structure is a bit too complicated
-data Plugin st = Plugin
-    -- If want its state to be saved on shutdown return a serializer, returns Nothing in default impl
-    { pluginSerialize :: !(Maybe (Serial st))
-
-    -- Default state, returns error in default impl
-    , pluginDefState :: !(PM st)
-
-    -- Initialize
-    , pluginInit :: !(PluginT st PM ())
-
-    -- Finalize
-    , pluginExit :: !(PluginT st PM ())
-
-    -- Listeners
-    , pluginListeners :: !(PluginT st PM [Command (PluginT st PM)])
-
-    -- TODO: do we want contextual listeners who can response to raw stream IRC Messages
-    }
 
 
---
--- Plugin Constructor
---
-newPlugin :: Plugin st
-newPlugin = Plugin
-    { pluginSerialize = Nothing
-    , pluginDefState  = return $ error "State not initalized"
-    , pluginInit      = return ()
-    , pluginExit      = return ()
-    , pluginListeners = return []
-    }
-
-
-
---
--- Generation Four
---
-data ExtensibleState = ExtensibleState (Map.Map String (Either String StateExtension))
-
-
--- ---------------------------------------------------------------------
--- Extensible state
---
-
--- | Every module must make the data it wants to store
--- an instance of this class.
---
--- Minimal complete definition: initialValue
-class Typeable a => ExtensionClass a where
-    -- | Defines an initial value for the state extension
-    initialValue :: a
-    -- | Specifies whether the state extension should be
-    -- persistent. Setting this method to 'PersistentExtension'
-    -- will make the stored data survive restarts, but
-    -- requires a to be an instance of Read and Show.
-    --
-    -- It defaults to 'StateExtension', i.e. no persistence.
-    extensionType :: a -> StateExtension
-    extensionType = StateExtension
-
--- | Existential type to store a state extension.
-data StateExtension =
-    forall a. ExtensionClass a => StateExtension a
-    -- ^ Non-persistent state extension
-  | forall a. (Read a, Show a, ExtensionClass a) => PersistentExtension a
-    -- ^ Persistent extension
-
-
-
---
--- Generation Two
---
-deriving instance Typeable ClockTime
-data PluginPlus = PluginPlus
-    -- Plugin Name
-    { nameP :: B.ByteString
-
-    -- Either initalize or loads a persisted state
-    , initP :: (Monad f, MonadIO f) => Maybe B.ByteString -> f Dynamic
-
-    -- Saves the persist state
-    , saveP :: Dynamic -> B.ByteString
-
-    -- Does the message match this plugin
-    , matchP :: Msg -> State Dynamic Bool
-
-    -- Evalulate the message and optionally return a result
-    , evalP :: (Monad f, MonadIO f) => Msg -> StateT Dynamic f (Maybe Msg)
-    }
-
--- Test implementation
-pingP = PluginPlus pingName pingInit pingSave pingMatch pingEval
-    where
-        pingName    = "Ping"
-        pingInit _  = return $ toDyn ()
-        pingSave _  = B.empty
-        pingMatch m = return $ m == "Hi"
-        pingEval m  = return $ Just m
-
-uptimeP = PluginPlus uptimeName uptimeInit uptimeSave uptimeMatch uptimeEval
-    where
-        uptimeName    = "Uptime"
-        uptimeInit _  = liftM toDyn (liftIO getClockTime)
-        uptimeSave _  = B.empty
-        uptimeMatch m = return $ m == "Bye"
-        uptimeEval m  = do
-            now  <- liftIO getClockTime
-            past <- get
-
-            return $ case fromDynamic past of
-                Nothing -> Nothing
-                Just p  -> Just $ pretty $ diffClockTimes now p
-
-
--- Supporting code for running and managing the state
-pluginP :: [PluginPlus]
-pluginP = [pingP, uptimeP]
-
--- TODO: Probably want to have sanity check or a way to enforce nameP to be unique
-initEmptyPluginData :: (Functor f, Monad f, MonadIO f) => [PluginPlus] -> f (Map.Map B.ByteString Dynamic)
-initEmptyPluginData l = liftM Map.fromList $ zip (map nameP l) `fmap` mapM (`initP` Nothing) l
-
-
---
--- Take advantage of lazy eval and maybe consider revert state to lazy
--- We just need to feed this into everything (maybe problemic with IO, need
--- double check)
-commandP :: (Monad f, MonadIO f)
-         => [PluginPlus]
-         -> Map.Map B.ByteString Dynamic
-         -> Msg
-         -> f (Maybe Msg, Map.Map B.ByteString Dynamic)
-commandP s m = undefined
---
--- Need a neat way to be able to take a list of match rule then upon the
--- first or whatever match, pass it on to the eval to evalulate it
---
--- I wonder if there's no neat way to just iterate through the plugin list
--- then run the match and then if it matches then eval then keep going... ?
---
-
-
-
---    -- Does the message match this plugin
---    , matchP :: Msg -> State Dynamic Bool
---
---    -- Evalulate the message and optionally return a result
---    , evalP :: (Monad f, MonadIO f) => Msg -> StateT Dynamic f (Maybe Msg)
 --    -- TODO: look into seeing if there's a way to setup some form of
 --    -- generic filtering rules such as "if chan = y send to x", etc..
 --    -- Perhaps Pipe.Prelude.Filter
