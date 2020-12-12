36b6f2082bc788ee3b95bac8270bd5ee516cf8ed
Anja Berens <pharaun666@gmail.com>
2020-12-12 02:58:09 -0800

    Updated schema files to insert more votes

--- schema/v3-v4.sql
+++ schema/v3-v4.sql
@@ -15,50 +15,130 @@ CREATE TABLE reacji_message (
     id INTEGER PRIMARY KEY NOT NULL,
     ts VARCHAR NOT NULL,
     chan_id INTEGER NOT NULL,
-    nick_id INTEGER DEFAULT NULL,
+    nick_id INTEGER NOT NULL,
     message VARCHAR NOT NULL,
     UNIQUE (ts, chan_id)
 );
 
-/* Add new reacji triggers */
+/* Triggers for reacji_votes table */
+/* Since action = 1 for add -1 for remove we'll be clever here and apply the action */
 CREATE TRIGGER increase_reacji_karma_count AFTER INSERT ON reacji_votes
   WHEN NEW.amount = 1 BEGIN
     INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
     UPDATE karma_given_count SET up = up + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET up = up + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
+    UPDATE karma_received_count SET up = up + NEW.action  WHERE name = (
+        SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+    );
+    UPDATE karma_received_count SET up = up + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
+  END;
+CREATE TRIGGER decrease_reacji_karma_count AFTER INSERT ON reacji_votes
+  WHEN NEW.amount = -1 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
+    UPDATE karma_given_count SET down = down + NEW.action WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET down = down + NEW.action
+        WHERE name = (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        );
+    UPDATE karma_received_count SET down = down + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
+  END;
+CREATE TRIGGER increase_sidevote_reacji_karma_count AFTER INSERT ON reacji_votes
+  WHEN NEW.amount = 0 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
+    UPDATE karma_given_count SET side = side + NEW.action WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET side = side + NEW.action
+        WHERE name = (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        );
+    UPDATE karma_received_count SET side = side + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
   END;
 
 CREATE TRIGGER delete_increase_reacji_karma_count_add AFTER DELETE ON reacji_votes
   WHEN OLD.amount = 1 BEGIN
     UPDATE OR IGNORE karma_given_count SET up = up - OLD.action  WHERE name = OLD.by_whom_name;
-    UPDATE OR IGNORE karma_received_count SET up = up - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
+    UPDATE OR IGNORE karma_received_count SET up = up - OLD.action
+        WHERE name = (
+            SELECT message FROM reacji_message where ID = OLD.reacji_message_id
+        );
+    UPDATE karma_received_count SET up = up - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
   END;
-
-CREATE TRIGGER decrease_reacji_karma_count AFTER INSERT ON reacji_votes
-  WHEN NEW.amount = -1 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET down = down + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET down = down + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
-  END;
-
 CREATE TRIGGER delete_decrease_reacji_karma_count_add AFTER DELETE ON reacji_votes
   WHEN OLD.amount = -1 BEGIN
-    UPDATE OR IGNORE karma_given_count SET down = down - OLD.action  WHERE name = OLD.by_whom_name;
-    UPDATE OR IGNORE karma_received_count SET down = down - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
+    UPDATE OR IGNORE karma_given_count SET down = down - OLD.action WHERE name = OLD.by_whom_name;
+    UPDATE OR IGNORE karma_received_count SET down = down - OLD.action
+        WHERE name = (
+            SELECT message FROM reacji_message where ID = OLD.reacji_message_id
+        );
+    UPDATE karma_received_count SET down = down - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
   END;
-
-CREATE TRIGGER increase_sidevote_reacji_karma_count AFTER INSERT ON reacji_votes
-  WHEN NEW.amount = 0 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET side = side + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET side = side + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
-  END;
-
 CREATE TRIGGER delete_increase_sidevote_reacji_karma_count_add AFTER DELETE ON reacji_votes
   WHEN OLD.amount = 0 BEGIN
     UPDATE OR IGNORE karma_given_count SET side = side - OLD.action  WHERE name = OLD.by_whom_name;
     UPDATE OR IGNORE karma_received_count SET side = side - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
+    UPDATE karma_received_count SET side = side - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
   END;
--- schema/v4.sql
+++ schema/v4.sql
@@ -63,73 +63,41 @@ CREATE TABLE karma_given_count (
 );
 
 
-/* Triggers for upvote on votes */
+/* Triggers for votes table */
 CREATE TRIGGER increase_karma_count AFTER INSERT ON votes
   WHEN NEW.amount = 1 BEGIN
     INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET up = up + 1 WHERE name = NEW.by_whom_name;
     INSERT OR IGNORE INTO karma_received_count VALUES (NULL, NEW.for_what_name, 0, 0, 0);
+
+    UPDATE karma_given_count SET up = up + 1 WHERE name = NEW.by_whom_name;
     UPDATE karma_received_count SET up = up + 1 WHERE name = NEW.for_what_name;
   END;
+CREATE TRIGGER decrease_karma_count AFTER INSERT ON votes
+  WHEN NEW.amount = -1 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, NEW.for_what_name, 0, 0, 0);
+
+    UPDATE karma_given_count SET down = down + 1 WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET down = down + 1 WHERE name = NEW.for_what_name;
+  END;
+CREATE TRIGGER increase_sidevote_count AFTER INSERT ON votes
+  WHEN NEW.amount = 0 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, NEW.for_what_name, 0, 0, 0);
+
+    UPDATE karma_given_count SET side = side + 1 WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET side = side + 1 WHERE name = NEW.for_what_name;
+  END;
+
 CREATE TRIGGER delete_increase_karma_count AFTER DELETE ON votes
   WHEN OLD.amount = 1 BEGIN
     UPDATE OR IGNORE karma_given_count SET up = up - 1 WHERE name = OLD.by_whom_name;
     UPDATE OR IGNORE karma_received_count SET up = up - 1 WHERE name = OLD.for_what_name;
   END;
-
-/* Triggers for reacji upvote */
-/* Since action = 1 for add -1 for remove we'll be clever here and apply the action */
-CREATE TRIGGER increase_reacji_karma_count AFTER INSERT ON reacji_votes
-  WHEN NEW.amount = 1 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET up = up + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET up = up + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
-  END;
-CREATE TRIGGER delete_increase_reacji_karma_count_add AFTER DELETE ON reacji_votes
-  WHEN OLD.amount = 1 BEGIN
-    UPDATE OR IGNORE karma_given_count SET up = up - OLD.action  WHERE name = OLD.by_whom_name;
-    UPDATE OR IGNORE karma_received_count SET up = up - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
-  END;
-
-
-/* Triggers for downvote on votes */
-CREATE TRIGGER decrease_karma_count AFTER INSERT ON votes
-  WHEN NEW.amount = -1 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET down = down + 1 WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, NEW.for_what_name, 0, 0, 0);
-    UPDATE karma_received_count SET down = down + 1 WHERE name = NEW.for_what_name;
-  END;
 CREATE TRIGGER delete_decrease_karma_count AFTER DELETE ON votes
   WHEN OLD.amount = -1 BEGIN
     UPDATE OR IGNORE karma_given_count SET down = down - 1 WHERE name = OLD.by_whom_name;
     UPDATE OR IGNORE karma_received_count SET down = down - 1 WHERE name = OLD.for_what_name;
-  END;
-
-/* Triggers for reacji downvote */
-/* Since action = 1 for add -1 for remove we'll be clever here and apply the action */
-CREATE TRIGGER decrease_reacji_karma_count AFTER INSERT ON reacji_votes
-  WHEN NEW.amount = -1 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET down = down + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET down = down + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
-  END;
-CREATE TRIGGER delete_decrease_reacji_karma_count_add AFTER DELETE ON reacji_votes
-  WHEN OLD.amount = -1 BEGIN
-    UPDATE OR IGNORE karma_given_count SET down = down - OLD.action  WHERE name = OLD.by_whom_name;
-    UPDATE OR IGNORE karma_received_count SET down = down - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
-  END;
-
-
-/* Triggers for sidevote on votes */
-CREATE TRIGGER increase_sidevote_count AFTER INSERT ON votes
-  WHEN NEW.amount = 0 BEGIN
-    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
-    UPDATE karma_given_count SET side = side + 1 WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, NEW.for_what_name, 0, 0, 0);
-    UPDATE karma_received_count SET side = side + 1 WHERE name = NEW.for_what_name;
   END;
 CREATE TRIGGER delete_increase_sidevote_count AFTER DELETE ON votes
   WHEN OLD.amount = 0 BEGIN
@@ -137,17 +105,126 @@ CREATE TRIGGER delete_increase_sidevote_count AFTER DELETE ON votes
     UPDATE OR IGNORE karma_received_count SET side = side - 1 WHERE name = OLD.for_what_name;
   END;
 
-/* Triggers for reacji side */
+
+/* Triggers for reacji_votes table */
 /* Since action = 1 for add -1 for remove we'll be clever here and apply the action */
+CREATE TRIGGER increase_reacji_karma_count AFTER INSERT ON reacji_votes
+  WHEN NEW.amount = 1 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
+    UPDATE karma_given_count SET up = up + NEW.action WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET up = up + NEW.action  WHERE name = (
+        SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+    );
+    UPDATE karma_received_count SET up = up + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
+  END;
+CREATE TRIGGER decrease_reacji_karma_count AFTER INSERT ON reacji_votes
+  WHEN NEW.amount = -1 BEGIN
+    INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
+    UPDATE karma_given_count SET down = down + NEW.action WHERE name = NEW.by_whom_name;
+    UPDATE karma_received_count SET down = down + NEW.action
+        WHERE name = (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        );
+    UPDATE karma_received_count SET down = down + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
+  END;
 CREATE TRIGGER increase_sidevote_reacji_karma_count AFTER INSERT ON reacji_votes
   WHEN NEW.amount = 0 BEGIN
     INSERT OR IGNORE INTO karma_given_count VALUES (NULL, NEW.by_whom_name, 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        ), 0, 0, 0);
+    INSERT OR IGNORE INTO karma_received_count
+        VALUES (NULL, (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+            ),
+        0, 0, 0);
+
     UPDATE karma_given_count SET side = side + NEW.action WHERE name = NEW.by_whom_name;
-    INSERT OR IGNORE INTO karma_received_count VALUES (NULL, (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id), 0, 0, 0);
-    UPDATE karma_received_count SET side = side + NEW.action  WHERE name = (SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id);
+    UPDATE karma_received_count SET side = side + NEW.action
+        WHERE name = (
+            SELECT message FROM reacji_message WHERE ID = NEW.reacji_message_id
+        );
+    UPDATE karma_received_count SET side = side + NEW.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = NEW.reacji_message_id
+        );
+  END;
+
+CREATE TRIGGER delete_increase_reacji_karma_count_add AFTER DELETE ON reacji_votes
+  WHEN OLD.amount = 1 BEGIN
+    UPDATE OR IGNORE karma_given_count SET up = up - OLD.action  WHERE name = OLD.by_whom_name;
+    UPDATE OR IGNORE karma_received_count SET up = up - OLD.action
+        WHERE name = (
+            SELECT message FROM reacji_message where ID = OLD.reacji_message_id
+        );
+    UPDATE karma_received_count SET up = up - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
+  END;
+CREATE TRIGGER delete_decrease_reacji_karma_count_add AFTER DELETE ON reacji_votes
+  WHEN OLD.amount = -1 BEGIN
+    UPDATE OR IGNORE karma_given_count SET down = down - OLD.action WHERE name = OLD.by_whom_name;
+    UPDATE OR IGNORE karma_received_count SET down = down - OLD.action
+        WHERE name = (
+            SELECT message FROM reacji_message where ID = OLD.reacji_message_id
+        );
+    UPDATE karma_received_count SET down = down - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
   END;
 CREATE TRIGGER delete_increase_sidevote_reacji_karma_count_add AFTER DELETE ON reacji_votes
   WHEN OLD.amount = 0 BEGIN
     UPDATE OR IGNORE karma_given_count SET side = side - OLD.action  WHERE name = OLD.by_whom_name;
     UPDATE OR IGNORE karma_received_count SET side = side - OLD.action WHERE name = (SELECT message FROM reacji_message where ID = OLD.reacji_message_id);
+    UPDATE karma_received_count SET side = side - OLD.action
+        WHERE name = (
+            SELECT cleaned_nick FROM nick_metadata
+                INNER JOIN reacji_message ON nick_metadata.id = reacji_message.nick_id
+                WHERE reacji_message.id = OLD.reacji_message_id
+        );
   END;
