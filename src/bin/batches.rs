0f937dcfc3953a999b13d4fd24214bcb024b8d3f
Anja Berens <pharaun666@gmail.com>
2022-05-11 17:56:56 -0700

    Improvement to batch and start of a prune batch script

--- src/bin/batches.rs
+++ src/bin/batches.rs
@@ -7,19 +7,20 @@ use std::path::Path;
 
 use std::error::Error;
 use std::vec::Vec;
+use std::ffi::OsString;
+use std::collections::HashSet;
+
+use chrono::Local;
+use chrono::DateTime;
+use chrono::Datelike;
+use glob::glob;
 
 fn main() {
-    let matches = App::new("Karmator db maintance batch")
+    let matches = App::new("Karmator maintance batch")
         .version(crate_version!())
         .author(crate_authors!("\n"))
-        .about("Handles the maintance work on the karmator db")
+        .about("Handles the maintance work for karmator")
         .setting(AppSettings::ColoredHelp)
-        .arg(
-            Arg::with_name("FILE")
-                .help("Database file to operate on")
-                .required(true)
-                .index(1),
-        )
         .subcommand(
             SubCommand::with_name("runs")
                 .about("Detect runs of votes")
@@ -33,18 +34,47 @@ fn main() {
                     Arg::with_name("delete")
                         .long("delete")
                         .help("Delete the runs detected"),
+                )
+                .arg(
+                    Arg::with_name("FILE")
+                        .help("Database file to operate on")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("prune")
+                .about("Prune and pack up old backups")
+                .arg(
+                    Arg::with_name("delete")
+                        .long("delete")
+                        .help("Delete the old files"),
+                )
+                .arg(
+                    Arg::with_name("BACKUPS")
+                        .help("Backup directory to prune")
+                        .required(true),
                 ),
         )
         .get_matches();
 
-    let filename = matches.value_of("FILE").unwrap_or("db.sqlite");
     match matches.subcommand() {
-        ("runs", Some(m)) => {
-            let _ = run(m, filename);
+        ("runs", Some(m))  => {
+            let filename = m.value_of("FILE").unwrap();
+            let min = value_t!(m, "min", u32).unwrap_or(20);
+            let delete = m.is_present("delete");
+
+            run(filename, min, delete)
         },
+        ("prune", Some(m)) => {
+            let directory = m.value_of("BACKUPS").unwrap();
+            let delete = m.is_present("delete");
+
+            prune(directory, delete)
+        }
         _ => {
             println!("{}", matches.usage());
-        }
+            Ok(())
+        },
     };
 }
 
@@ -93,10 +123,7 @@ fn str_amount(amount: i8) -> &'static str {
     }
 }
 
-fn run(matches: &ArgMatches, filename: &str) -> Result<(), Box<dyn Error>> {
-    let min = value_t!(matches, "min", u32).unwrap_or(20);
-    let delete = matches.is_present("delete");
-
+fn run(filename: &str, min: u32, delete: bool) -> Result<(), Box<dyn Error>> {
     let conn =
         rs::Connection::open_with_flags(Path::new(filename), rs::OpenFlags::SQLITE_OPEN_READ_WRITE)
             .expect(&format!("Connection error: {}", filename));
@@ -186,4 +213,73 @@ fn run(matches: &ArgMatches, filename: &str) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+
+fn prune(directory: &str, delete: bool) -> Result<(), Box<dyn Error>> {
+    let now: DateTime<Local> = Local::now();
+    let year = now.year();
+    let month = now.month();
+
+    // Fetch a set of all of the file in the current month+year
+    let current_month_year = collect_glob(directory, &format!("/db-backup-{}-{:02}-??.sqlite.zst", year, month));
+
+    // Fetch a set of all of the file that is in previous year + first of the month
+    // If == 12, pack it up
+    let previous_first_month = collect_glob(directory, &format!("/db-backup-{}-??-01.sqlite.zst", year - 1));
+
+    // Fetch a set of all of the file that is in current year + first of the month
+    // if == 12 pack it up
+    let current_first_month = collect_glob(directory, &format!("/db-backup-{}-??-01.sqlite.zst", year));
+
+    // Fetch a set of all of the files
+    let all_files = collect_glob(directory, "/db-backup-????-??-??.sqlite.zst");
+
+    println!("cmy: {:?}\n", current_month_year);
+    println!("pfm: {:?}\n", previous_first_month);
+    println!("cfm: {:?}\n", current_first_month);
+    println!("All: {:?}\n", all_files);
+
+    // Calculate what to keep and what to delete
+    let delete = delete_set(&all_files, vec![&current_month_year, &previous_first_month, &current_first_month]);
+
+    println!("DELETE: {:?}\n", delete);
+
+
+    println!("cmy: {:?}", current_month_year.len());
+    println!("pfm: {:?}", previous_first_month.len());
+    println!("cfm: {:?}", current_first_month.len());
+    println!("keeping: {:?}\n", current_month_year.len() + previous_first_month.len() + current_first_month.len());
+
+    println!("All: {:?}", all_files.len());
+    println!("DELETE: {:?}", delete.len());
+    println!("keeping: {:?}", all_files.len() - delete.len());
+    println!("Difference is cos of double counting the current_first_month + current_month_year's first day file");
+
+    // Rules:
+    //  - Keep current month of daily snapshot
+    //  - Keep first of each month in a year
+    //  - When there is 12 first month snapshot of a year, compact it into a tarfile
+
+
+
+    Ok(())
+}
+
+fn collect_glob(directory: &str, glob_str: &str) -> HashSet<OsString> {
+    glob(&(directory.to_string() + glob_str)).unwrap()
+        .flatten()
+        .flat_map(|e| e.file_name().map(|s| s.to_os_string()))
+        .collect::<HashSet<OsString>>()
+}
+
+fn delete_set(all: &HashSet<OsString>, keep: Vec<&HashSet<OsString>>) -> HashSet<OsString> {
+    let mut delete = all.clone();
+
+    for hs in keep {
+        let out = delete.difference(&hs).map(|e| e.clone()).collect();
+        delete = out;
+    }
+
+    delete
 }
