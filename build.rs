bdc82021e5d771eb4920095d118deff8669659fe
Anja Berens <pharaun666@gmail.com>
2024-12-25 15:27:46 -0600

    Bump all dependencies

--- build.rs
+++ build.rs
@@ -4,10 +4,5 @@ use std::env;
 fn main() {
     let src = env::var("CARGO_MANIFEST_DIR").unwrap();
     let dst = Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
-    let mut opts = built::Options::default();
-    opts.set_env(true);
-    opts.set_git(true);
-    opts.set_time(true);
-
-    built::write_built_file_with_opts(&opts, src.as_ref(), &dst).expect("Failed to build time info");
+    built::write_built_file_with_opts(Some(src.as_ref()), &dst).expect("Failed to build time info");
 }
